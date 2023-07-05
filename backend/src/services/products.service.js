9c1e81751054d97f530c424ef7adc64f41cee861
AlissonSeraphim <projetoseraphim@gmail.com>
2023-07-04 23:34:13 -0300

    feat: added business validations for product registration

--- backend/src/services/products.service.js
+++ backend/src/services/products.service.js
@@ -13,9 +13,15 @@ const findById = async (ProductId) => {
 };
 
 const insertProduct = async (productName) => {
-  if (!productName) return { status: 'INVALID_VALUE', data: { message: 'Product name Invalid' } };
+  if (!productName) return { status: 'BAD_REQUEST', data: { message: '"name" is required' } };
+
+  if (productName.length < 5) {
+    const InvalidMessage = '"name" length must be at least 5 characters long';
+    return { status: 'INVALID_VALUE', data: { message: InvalidMessage } };
+  }
 
   const insertedProduct = await productsModel.insertProduct(productName);
+
   return { status: 'CREATED', data: insertedProduct };
 };
 
